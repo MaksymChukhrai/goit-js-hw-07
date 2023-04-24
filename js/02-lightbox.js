a926beb26c12f2f4468068d62d3b23b3203f22eb
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2023-04-24 04:49:48 +0300

    Add files via upload

---
+++ js/02-lightbox.js
@@ -0,0 +1,24 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+
+const galleryList = document.querySelector(".gallery");
+
+const liElements = galleryItems.map(
+  (item) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+    </a>
+  </li>
+`
+);
+
+galleryList.insertAdjacentHTML('beforeend', liElements.join(''));
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250
+});
+
+
